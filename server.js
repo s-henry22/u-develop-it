975339e83df09ac9a077d4969ff8974dc611e7e8
Spencer Henry <spencerhenry94@gmail.com>
2021-05-13 21:30:12 -0400

    created query statements

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -9,44 +10,45 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'Toronto!22',
-      database: 'election'
-    },
-    console.log('Connected to the election database.')
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Toronto!22',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
 );
 
+// Retreives all data from candidate table
 /*db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+  console.log(rows);
 });*/
 
 // GET a single candidate
 /*db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(row);
+  if (err) {
+    console.log(err);
+  }
+  console.log(row)
 });*/
 
 // Delete a candidate
 /*db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(result);
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });*/
 
 // Create a candidate
-/*const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+/*const sql = `INSERT INTO candidates (ID, first_name, last_name, industry_connected)
               VALUES (?,?,?,?)`;
 const params = [1, 'Ronald', 'Firbank', 1];
 
 db.query(sql, params, (err, result) => {
-  if (err) {
+  if(err) {
     console.log(err);
   }
   console.log(result);
@@ -56,7 +58,6 @@ db.query(sql, params, (err, result) => {
 app.use((req, res) => {
     res.status(404).end();
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
